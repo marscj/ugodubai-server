386598f2622a2d9419c5a6c549dd8c0c5f4ec634
marscj <mars.jinxing@gmail.com>
2023-06-22 15:50:25 +0400

    commit

--- api/v1/system/sys_user.go
+++ api/v1/system/sys_user.go
@@ -47,10 +47,10 @@ type UserGetParamsRes struct {
 
 // SetUserReq 添加修改用户公用请求字段
 type SetUserReq struct {
-	DeptId   uint64  `p:"deptId" v:"required#用户部门不能为空"` //所属部门
-	Email    string  `p:"email" v:"email#邮箱格式错误"`       //邮箱
-	NickName string  `p:"nickName" v:"required#用户昵称不能为空"`
-	Mobile   string  `p:"mobile" v:"required|phone#手机号不能为空|手机号格式错误"`
+	DeptId   uint64  `p:"deptId"`                 //所属部门
+	Email    string  `p:"email" v:"email#邮箱格式错误"` //邮箱
+	NickName string  `p:"nickName" `
+	Mobile   string  `p:"mobile"`
 	PostIds  []int64 `p:"postIds"`
 	Remark   string  `p:"remark"`
 	RoleIds  []int64 `p:"roleIds"`
